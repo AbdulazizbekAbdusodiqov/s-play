799d75e2911c82a4c5ec4b8e788c5f7ca8444fb5
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-03-01 13:56:51 +0500

    add category, genres, genres_images, language, payment_method, profile, subscription, subscription_plans

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,14 @@ import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from './prisma/prisma.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from "./auth/auth.module";
+import { LanguageModule } from './language/language.module';
+import { ProfileModule } from './profile/profile.module';
+import { CategoryModule } from './category/category.module';
+import { GenresModule } from './genres/genres.module';
+import { GenresImagesModule } from './genres_images/genres_images.module';
+import { PaymentMethodModule } from './payment_method/payment_method.module';
+import { SubscriptionPlansModule } from './subscription_plans/subscription_plans.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
     imports: [
@@ -12,7 +20,15 @@ import { AuthModule } from "./auth/auth.module";
     }),
       PrismaModule,
       AdminModule,
-      AuthModule
+      AuthModule,
+      LanguageModule,
+      ProfileModule,
+      CategoryModule,
+      GenresModule,
+      GenresImagesModule,
+      PaymentMethodModule,
+      SubscriptionPlansModule,
+      SubscriptionModule
     ],
     controllers: [],
     providers: [],
